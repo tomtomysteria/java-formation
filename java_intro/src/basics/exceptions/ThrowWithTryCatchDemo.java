51bdeefa8c5987d2832c818315d58aadd8dcb338
Tom <pro.cohesia@gmail.com>
2025-05-13 10:41:37 +0200

    style(day3): add comments about exceptions

--- java_intro/src/basics/exceptions/ThrowWithTryCatchDemo.java
+++ java_intro/src/basics/exceptions/ThrowWithTryCatchDemo.java
@@ -3,12 +3,15 @@ package basics.exceptions;
 public class ThrowWithTryCatchDemo {
   public static void validateAge(int age) {
     if (age < 18) {
-      throw new IllegalArgumentException("Âge insuffisant !");
+      throw new IllegalArgumentException("Âge insuffisant !"); // Lève l'erreur → signaler un problème
     }
     System.out.println("Accès autorisé.");
   }
 
   public static void main(String[] args) {
+    // Gère l'erreur → empêcher un plantage.
+    // Le bloc catch s'exécute seulement si l’exception levée correspond au type
+    // d’exception capturée.
     try {
       validateAge(15); // Lève (throw) une exception
     } catch (IllegalArgumentException e) {
