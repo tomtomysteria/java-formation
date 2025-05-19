d3e3d0b29beee80f54f98a1bca92072114085580
Tom <pro.cohesia@gmail.com>
2025-05-19 11:59:46 +0200

    feat(day13): add logger

--- microservices/product-service/src/main/java/com/example/productservice/controller/ProductController.java
+++ microservices/product-service/src/main/java/com/example/productservice/controller/ProductController.java
@@ -5,9 +5,14 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+  private static final Logger logger = LogManager.getLogger(ProductController.class);
 
   @GetMapping("/{id}")
   public Product getProduct(@PathVariable Long id) {
@@ -16,6 +21,8 @@ public class ProductController {
 
   @GetMapping
   public List<Product> getAllProducts() {
+    logger.info("Get products");
+    logger.debug("Test DEBUG log");
     return List.of(
         new Product(1L, "Chaise", 49.99),
         new Product(2L, "Table", 89.99));
