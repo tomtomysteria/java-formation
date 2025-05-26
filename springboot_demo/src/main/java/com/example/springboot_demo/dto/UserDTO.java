89bfa69d55646494ea9234dbb3c51e8ab37e07b1
Tom <pro.cohesia@gmail.com>
2025-05-26 12:40:50 +0200

    feat(springboot_demo): implement dto to handle password in create and uodate user requests

---
+++ springboot_demo/src/main/java/com/example/springboot_demo/dto/UserDTO.java
@@ -0,0 +1,35 @@
+package com.example.springboot_demo.dto;
+
+import java.util.Set;
+
+public class UserDTO {
+
+  private String username;
+  private String password;
+  private Set<String> roles;
+
+  // Getters and Setters
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Set<String> getRoles() {
+    return roles;
+  }
+
+  public void setRoles(Set<String> roles) {
+    this.roles = roles;
+  }
+}
