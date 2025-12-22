373b4af7cbe3b6df1ec5ba1e858df9db2db83665
Thomas Cesbron <pro.cohesia@gmail.com>
2025-12-22 09:36:55 +0100

    fix(app): minor improvements

--- user-management-frontend/src/main.server.ts
+++ user-management-frontend/src/main.server.ts
@@ -1,7 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import type { BootstrapContext } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
-
-export default bootstrap;
+export default function bootstrap(context: BootstrapContext) {
+  return bootstrapApplication(AppComponent, config, context);
+}
